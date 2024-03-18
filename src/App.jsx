92de9ddb9c2bcb255ff9e67eb34dc9f2eaeb9ea0
Bohao Wang <bj050323@gmail.com>
2024-03-18 21:02:05 +0800

    feat: add editable state

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,73 @@
-import { useState, useRef, useEffect } from 'react';
-import { Stage, Layer, Group, Text, Transformer, Rect } from 'react-konva';
+import { useState, useRef, useEffect } from "react";
+import { Stage, Layer, Group, Text, Transformer, Rect } from "react-konva";
 
-function Rectangle({ state, setState, index, isSelected, onSelect }) {
+function Rectangle({
+  state,
+  setState,
+  index,
+  isSelected,
+  onSelect,
+  onEdit,
+  onSubmit,
+}) {
   const shapeRef = useRef();
   const trRef = useRef();
+  const [edit, setEdit] = useState(false);
+  const editable = isSelected && edit;
 
   useEffect(() => {
-    if (isSelected) {
+    if (editable) {
       // we need to attach transformer manually
       trRef.current.nodes([shapeRef.current]);
       trRef.current.getLayer().batchDraw();
     }
-  }, [isSelected]);
+  }, [editable]);
+
+  if (!isSelected && edit) {
+    setEdit(false);
+  }
 
   return (
     <>
       <Group
         x={state.x}
         y={state.y}
-        draggable
-        onDragStart={() => {
-          setState({
-            ...state,
-            isDragging: true,
-          });
-        }}
+        draggable={editable}
         onDragEnd={(e) => {
           setState({
             ...state,
-            isDragging: false,
             x: e.target.x(),
             y: e.target.y(),
           });
         }}
       >
-        <Text y={-20} text={`模块${index + 1}`} />
-        <Text x={50} y={-20} text={`(${state.x},${state.y}) ${state.width}x${state.height}`} />
+        <Text y={-20} text={`模块${index + 1}`} onClick={onSelect} />
+        {isSelected && (
+          <>
+            <Text
+              x={50}
+              y={-20}
+              text={`(${state.x},${state.y}) ${state.width}x${state.height}`}
+            />
+            <Text
+              x={0}
+              y={state.height + 10}
+              text={edit ? "确认" : "编辑"}
+              onClick={() => {
+                if (edit) onSubmit();
+                else onEdit();
+                setEdit(!edit);
+              }}
+            />
+          </>
+        )}
         <Rect
           ref={shapeRef}
           x={0}
           y={0}
-          width={state.width} height={state.height} stroke="green"
+          width={state.width}
+          height={state.height}
+          stroke="green"
           onClick={onSelect}
           onTransformEnd={() => {
             // transformer is changing scale of the node
@@ -64,12 +91,12 @@ function Rectangle({ state, setState, index, isSelected, onSelect }) {
           }}
         />
       </Group>
-      {isSelected && (
+      {editable && (
         <Transformer
           ref={trRef}
           flipEnabled={false}
           rotateEnabled={false}
-          enabledAnchors={['middle-right', 'bottom-right', 'bottom-center']}
+          enabledAnchors={["middle-right", "bottom-right", "bottom-center"]}
           boundBoxFunc={(oldBox, newBox) => {
             // limit resize
             if (Math.abs(newBox.width) < 5 || Math.abs(newBox.height) < 5) {
@@ -87,58 +114,74 @@ function Main() {
   const [rects, setRects] = useState([
     {
       id: 0,
-      isDragging: false,
       x: 50,
       y: 50,
       width: 100,
       height: 100,
-    }
+    },
   ]);
   const [selectedId, setSelectedId] = useState(null);
+  const [tempRect, setTempRect] = useState(null);
 
-  const checkDeselect = (e) => {
-    const clickedOnEmpty = e.target === e.target.getStage();
-    if (clickedOnEmpty) setSelectedId(null);
+  const selectRect = (item) => {
+    if (selectedId !== item.id && tempRect) {
+      console.log("restore", selectedId, tempRect);
+      const index = rects.findIndex((x) => x.id === selectedId);
+      rects[index] = tempRect;
+      setTempRect(null);
+      setRects([...rects]);
+    }
+    setSelectedId(item.id);
   };
 
   const addCanvas = () => {
-    setRects(prev => [...prev, {
-      id: Date.now(),
-      isDragging: false,
-      x: 50,
-      y: 50,
-      width: 100,
-      height: 100,
-    }])
-  }
+    setRects((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        x: 50,
+        y: 50,
+        width: 100,
+        height: 100,
+      },
+    ]);
+  };
 
   const onChange = (index, data) => {
-    console.log('onchange', index, data);
+    console.log("onchange", index, data);
     rects[index] = data;
-    setRects([...rects])
-  }
+    setRects([...rects]);
+  };
 
   return (
     <div>
       <h1>Add To Canvas</h1>
       <button onClick={addCanvas}>Add Item</button>
       <h2>Canvas</h2>
-      <div style={{ border: '1px solid #eee' }}>
-        <Stage width={window.innerWidth} height={window.innerHeight} onMouseDown={checkDeselect}>
+      <div style={{ border: "1px solid #eee" }}>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
           <Layer>
             {rects.map((rect, index) => (
               <Rectangle
-                key={rect.id} state={rect} index={index}
+                key={rect.id}
+                state={rect}
+                index={index}
                 isSelected={rect.id === selectedId}
-                onSelect={() => setSelectedId(rect.id)}
                 setState={(data) => onChange(index, data)}
+                onSelect={() => selectRect(rect)}
+                onEdit={() => {
+                  setTempRect({ ...rects[index] });
+                }}
+                onSubmit={() => {
+                  setTempRect(null);
+                }}
               />
             ))}
           </Layer>
         </Stage>
       </div>
     </div>
-  )
+  );
 }
 
 export default Main;
